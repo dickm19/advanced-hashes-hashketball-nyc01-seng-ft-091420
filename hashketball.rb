9d6f62d9d8f45e4103b9692c9d6cbe7a49cb077b
Mary Dick <maryclairedick@gmail.com>
2020-09-02 18:48:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,8 +131,9 @@ end
 def num_points_scored(player_name)
   game_hash
   game_hash.each do |team, info|
-    binding.pry
+    #binding.pry
     info[:players].each do |player|
+      binding.pry
       if :player_name == player_name
         return info[player][:points]
       end
